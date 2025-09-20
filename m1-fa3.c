77944562137b797abc1be0ae600a115af81ee743
LiveGekko23 <159462655+GeloDaet07@users.noreply.github.com>
2025-09-20 09:29:48 +0800

    Add files via upload

--- m1-fa3.c
+++ m1-fa3.c
@@ -25,13 +25,13 @@ int main() {
     if (jpt > 0){
         close(fd[1]);
         read(fd[0], buffer, sizeof(buffer));
+	printf("Parent jpt: %d\n", getpid());
+        printf("Child jpt: %d\n", jpt);
         printf("Parent received message: %s\n", buffer);
         close(fd[0]);
         wait(NULL);
     } else {
         close(fd[0]);
-        printf("Parent jpt: %d\n", getpid());
-        printf("Child jpt: %d\n", jpt);
         write(fd[1], message, strlen(message) + 1);
         close(fd[1]);
         exit(0);
